f7f41f6ee5278555a51764325a9046d29b5d23d8
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-23 18:58:48 +0200

    complete infreestructure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './redux/store';
 import './App.css';
 
-async function App() {
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Buscador de Peliculas</h1>
-        <p>Cabecera inicial, despues de limpiar el template</p>
-      </header>
-      <main>
-        <p>Aqui va ir el contenido principal</p>
-      </main>
-    </div>
+    <Provider store={configureStore()}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Buscador de Peliculas</h1>
+          <p>Cabecera inicial, despues de limpiar el template</p>
+        </header>
+        <main>
+          <p>Aqui va ir el contenido principal</p>
+        </main>
+      </div>
+    </Provider>
   );
 }
 
