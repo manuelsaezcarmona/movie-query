9ff9c20ed6ff1fd5b7c800576fb6cb6067a543f0
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-24 18:49:55 +0200

    style Favourites.js

--- src/components/Favourites/Favourites.js
+++ src/components/Favourites/Favourites.js
@@ -1,15 +1,20 @@
 import MovieCard from '../MovieCard/MovieCard';
+import styles from './styles.module.css';
+import { resultMoviesPage } from '../../services/mockdata';
 
 export default function Favourites() {
+  const favourites = resultMoviesPage.results;
   return (
     <main className="main-container">
-      <h2>Mis Peliculas Favoritas</h2>
-      <section className="favourites">
-        <p className="favourites__text">No se han encontrado Peliculas</p>
-        <ul className="favourites__list">
-          <li className="favourites__item">
-            <MovieCard />
-          </li>
+      <h2 className={styles.header}>Mis Peliculas Favoritas</h2>
+      <section className={styles.favourites}>
+        <p className={styles.favourites__text}>No tienes peliculas Favoritas</p>
+        <ul className={styles.favourites__list}>
+          {favourites.map((movie) => (
+            <li className={styles.favourites__item} key={`${movie.id}_fvt`}>
+              <MovieCard movie={movie} />
+            </li>
+          ))}
         </ul>
       </section>
     </main>
