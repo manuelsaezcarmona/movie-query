2d3786f42386ef333c70121e8e4ea4bb0b0e882b
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-24 11:16:47 +0200

    create infraestructure to global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
-import './App.css';
 
 import AppRouter from './routes/AppRouter';
 
