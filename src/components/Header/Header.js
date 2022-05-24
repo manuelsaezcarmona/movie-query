3b5028dabd25c1575cf8be5c5b7746ead3a6760b
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-24 12:46:55 +0200

    style header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom';
+import styles from './styles.module.css';
 
 export default function Header() {
   return (
-    <header className="App-header">
-      <h1 className="App-header__title">TMDB Searcher</h1>
-      <nav className="App-header__nav">
-        <ul className="App-header__list">
-          <li className="App-header__item">
-            <Link to="/" className="App-header__link">
+    <header className={styles['App-header']}>
+      <h1 className={styles['App-header__title']}>TMDB Searcher</h1>
+      <nav className={styles['App-header__nav']}>
+        <ul className={styles['App-header__list']}>
+          <li className={styles['App-header__item']}>
+            <Link to="/" className={styles['App-header__link']}>
               HomePage
             </Link>
           </li>
-          <li className="App-header__item">
-            <Link to="/favs" className="App-header__link">
+          <li className={styles['App-header__item']}>
+            <Link to="/favs" className={styles['App-header__link']}>
               Favourites
             </Link>
           </li>
