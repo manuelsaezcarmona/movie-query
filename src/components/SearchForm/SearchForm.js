94b786bf6d36b8026031262da1f6283d1de58c43
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-25 12:20:23 +0200

    add useForm hook

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 export default function SearchForm() {
   return (
     <div className={styles.search}>
-      <form action="" className={styles.search__form}>
+      <form className={styles.search__form}>
         <input className={styles.search__inputtext} type="text" name="search" placeholder="Busca tu pelicula" />
         <button className={styles.search__button} type="button">
           <GoSearch className={styles.search__icon} />
--- src/services/constants.js
+++ src/services/constants.js
@@ -5,6 +5,7 @@
 export const API_URL = 'https://api.themoviedb.org/3';
 export const API_KEY = '8f781d70654b5a6f2fa69770d1d115a3';
 export const DEFAULT_LANGUAGE = 'es-ES';
+export const DEFAULT_VALUE = 'Avengers';
 
 // Image full Path
 export const IMAGE_URL = 'https://image.tmdb.org/t/p';
