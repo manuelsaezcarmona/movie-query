5364356f763e30484386e2b89615b51c2c194e22
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-25 12:29:27 +0200

    useForm and handleSubmit in SearchForm.js

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,29 @@
 import { GoSearch } from 'react-icons/go';
+import { useForm } from '../../hooks/useForm';
 import styles from './styles.module.css';
 
 export default function SearchForm() {
+  const [formValues, handleInputChange] = useForm({ search: '' });
+
+  const { search } = formValues;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+  };
+
   return (
     <div className={styles.search}>
       <form className={styles.search__form}>
-        <input className={styles.search__inputtext} type="text" name="search" placeholder="Busca tu pelicula" />
-        <button className={styles.search__button} type="button">
+        <input
+          className={styles.search__inputtext}
+          type="text"
+          name="search"
+          value={search}
+          onChange={handleInputChange}
+          placeholder="Busca tu pelicula"
+        />
+        <button className={styles.search__button} type="button" onClick={handleSubmit}>
           <GoSearch className={styles.search__icon} />
         </button>
       </form>
