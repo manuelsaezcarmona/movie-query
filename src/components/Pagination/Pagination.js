80ec10dee82e2e13d51707266d18ab3b74e2e98b
Manuel Saez Carmona <70754764+manuelsaezcarmona@users.noreply.github.com>
2022-05-26 11:56:36 +0200

    add dispatchers previous & next to Pagination component

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,22 +1,40 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-// import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { nextPage, previousPage } from '../../redux/actions/settings';
+import { MINIMUM_PAGE } from '../../services/constants';
 
 export default function Pagination() {
+  const { currentpage, totalpages } = useSelector((state) => state.settings);
+  const dispatch = useDispatch();
+
   const handlePreviousClick = () => {
-    console.log('handlePreviousClick');
+    dispatch(previousPage(currentpage));
+    console.log(currentpage);
   };
 
   const handleNextClick = () => {
-    console.log('handleNextClick');
+    dispatch(nextPage(currentpage));
+    console.log(currentpage);
   };
 
   return (
     <div className="pagination">
-      <button type="button" onClick={handlePreviousClick}>
+      <button
+        type="button"
+        className="pagination__button"
+        id="prevButton"
+        onClick={handlePreviousClick}
+      >
         Previous
       </button>
-      <p>2</p>
-      <button type="button" onClick={handleNextClick}>
+      <p className="pagination__currentpage">{currentpage}</p>
+      <button
+        type="button"
+        className="pagination__button"
+        id="nextButton"
+        onClick={handleNextClick}
+      >
         Next
       </button>
     </div>
